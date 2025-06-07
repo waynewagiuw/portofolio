3e8688ad8763979452af8e5be849495c7762f8f9
waynewwagiuw <s22110290@student.unklab.ac.id>
2025-06-07 13:16:38 +0800

    image skills fixed

--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -3,7 +3,7 @@ const SubSkills = () => {
     <div className="border-y-2 border-lightGrey relative ">
       <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
       <img
-        src="../../public/images/subSkills.jpg"
+        src="/images/subSkills.jpg"
         alt="subSkills image"
         className=""
       />
