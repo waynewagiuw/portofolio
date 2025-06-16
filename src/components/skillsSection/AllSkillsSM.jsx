20a55f6d325822c9e661763ada38bf258bd73455
waynewwagiuw <s22110290@student.unklab.ac.id>
2025-06-16 22:11:26 +0800

    change skills

--- src/components/skillsSection/AllSkillsSM.jsx
+++ src/components/skillsSection/AllSkillsSM.jsx
@@ -1,47 +1,40 @@
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { SiTypescript } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { SiRedux } from "react-icons/si";
-import { SiNextdotjs } from "react-icons/si";
-import { RiTailwindCssFill } from "react-icons/ri";
+import { FaHtml5, FaGitAlt, FaDatabase, FaCss3Alt, FaReact } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 
+
 const skills = [
+  
   {
-    skill: "HTML",
-    icon: FaHtml5,
-  },
-  {
-    skill: "CSS",
-    icon: FaCss3Alt,
-  },
-  {
-    skill: "JavaScript",
-    icon: IoLogoJavascript,
-  },
-  {
-    skill: "TypeScript",
-    icon: SiTypescript,
-  },
-  {
-    skill: "ReactJS",
-    icon: FaReact,
-  },
-  {
-    skill: "Redux",
-    icon: SiRedux,
-  },
-  {
-    skill: "NextJS",
-    icon: SiNextdotjs,
-  },
-  {
-    skill: "TailwindCSS",
-    icon: RiTailwindCssFill,
-  },
+      skill: "HTML",
+      icon: FaHtml5,
+    },
+    {
+      skill: "CSS",
+      icon: FaCss3Alt,
+    },
+    {
+      skill: "JavaScript",
+      icon: IoLogoJavascript,
+    },
+    {
+      skill: "TypeScript",
+      icon: SiTypescript,
+    },
+    {
+      skill: "ReactJS",
+      icon: FaReact,
+    },
+    {
+      skill: "PostgreSQL",
+      icon: FaDatabase,
+    },
+    {
+      skill: "Git",
+      icon: FaGitAlt,
+    },
 ];
 
 const AllSkillsSM = () => {
