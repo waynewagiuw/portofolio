ec5cf95f7e3e972400ffb327a3542626a27e10e8
waynewwagiuw <s22110290@student.unklab.ac.id>
2025-06-30 15:14:06 +0800

    adding experience nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
 import ContactMeMain from "./components/contactMeSection/ContactMeMain";
 import ExperienceMain from "./components/experienceSection/ExperienceMain";
-import FooterGradientSM from "./components/footer/FooterGradientSM";
+import FooterGradient from "./components/footer/FooterGradient copy";
 import FooterMain from "./components/footer/FooterMain";
 import HeroGradient from "./components/heroSection/HeroGradient";
 import HeroMain from "./components/heroSection/HeroMain";
@@ -25,7 +25,7 @@ function App() {
       <ProjectsMain />
       <ContactMeMain />
       <FooterMain />
-      <FooterGradientSM />
+      <FooterGradient/>
     </main>
   );
 }
--- src/components/footer/FooterGradient copy.jsx
+++ src/components/footer/FooterGradient copy.jsx
@@ -1,10 +1,10 @@
 const FooterGradient = () => {
   return (
     <div>
-      <div className="  shadow-cyanMediumShadow absolute bottom-0 left-[400px] -z-10 animate-pulse"></div>
-      <div className="  shadow-cyanMediumShadow absolute bottom-[5%] right-0 -z-10 opacity-50"></div>
+      <div className="  shadow-cyanMediumShadow absolute bottom-0 right-0 -z-10 opacity-50"></div>
+      <div className="  shadow-cyanMediumShadow absolute -bottom-[5%] left-[35%] -z-10 opacity-50"></div>
       <div className="  shadow-orangeMediumShadow absolute bottom-0 left-0 -z-10 animate-pulse"></div>
-      <div className="  shadow-orangeMediumShadow absolute bottom-[10%] right-0 -z-10 opacity-50 "></div>
+      <div className="  shadow-orangeMediumShadow absolute -bottom-[5%] right-[35%] -z-10 opacity-50 "></div>
     </div>
   );
 };
